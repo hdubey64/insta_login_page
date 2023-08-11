3cb88e3cecbd522f3b0a5bbf02f0f350a70f1f8f
Vishal Dubey <hdubey64@gmail.com>
2023-08-11 12:33:20 +0530

    Compenets Folder Setup Done

---
+++ src/components/Card/index.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Card = (props) => {
+   return (
+      <>
+         <section>
+            <div className="main"></div>
+         </section>
+      </>
+   );
+};
+
+export default Card;
