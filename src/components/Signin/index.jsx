2de51b853962b6f9eec6d72cd67d77939529d49b
Vishal Dubey <hdubey64@gmail.com>
2023-08-12 12:22:50 +0530

    SignUP

---
+++ src/components/Signin/index.jsx
@@ -0,0 +1,12 @@
+const Signup = () => {
+   return (
+      <div className="userForm">
+         <input type="text" placeholder="Phone number,or email" />
+         <input type="text" placeholder="Full Name" />
+         <input type="text" placeholder="Phone number, username, or email" />
+         <input type="password" placeholder="Password" />
+      </div>
+   );
+};
+
+export default Signup;
